702edb06d367308311d3b4e6a6b3efaf46987a24
vivekel015 <vivekkumar@externlabs.com>
2022-04-11 10:54:11 +0530

    first commit

---
+++ test/token-test.js
@@ -0,0 +1,20 @@
+
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Greeter", function () {
+  it("Should return the new greeting once it's changed", async function () {
+    const Token = await ethers.getContractFactory("WToken");
+    const token = await Token.deploy("Hello, world!");
+    await token.deployed();
+
+    expect(await token.greet()).to.equal("Hello, world!");
+
+    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+
+    // wait until the transaction is mined
+    await setGreetingTx.wait();
+
+    expect(await greeter.greet()).to.equal("Hola, mundo!");
+  });
+});
